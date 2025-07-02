59c7e1d1c18547a87ad84c653f8d69cfbd6c21ef
Felipe <rocha.felipe98@gmail.com>
2025-07-02 14:08:18 -0300

    Revisando controller e policy

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,16 +1,26 @@
 class Availability < ApplicationRecord
- has_many :reservations, dependent: :destroy
- belongs_to :battery
+  has_many :reservations, dependent: :destroy
+  belongs_to :battery
 
- validates :date, uniqueness: { scope: [:time, :battery_id], message: "já possui disponibilidade nesse horário para essa pista" }
+  validates :date, uniqueness: { scope: [:time, :battery_id], message: "já possui disponibilidade nesse horário para essa pista" }
+  validates :total_slots, numericality: { greater_than: 0 }
+  validate :not_on_holiday
 
- validates :total_slots, numericality: { greater_than: 0 }
-
- def reserved_slots
+  def reserved_slots
     reservations.sum(:slot_count)
- end
+  end
 
- def available_slots
+  def available_slots
     total_slots - reserved_slots
- end
+  end
+
+  private
+
+  def not_on_holiday
+    return unless date.present?
+
+    if Holidays.on(date, :br_pe).any?
+      errors.add(:date, "não pode ser um feriado")
+    end
+  end
 end
